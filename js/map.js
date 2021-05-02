b8b6d6063434fb75d746fd9c7dbadd230e4623aa
H0R15H0 <shohori13@gmail.com>
2021-05-03 00:31:39 +0900

    CurrentPositionを表示　＋　円から四角形に変更

--- js/map.js
+++ js/map.js
@@ -1,7 +1,26 @@
+let map;
+let marker
+
+let currentLocation = { lat: 0, lng: 0}
+
+function changeCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(function location(position) {
+    currentLocation.lat = position.coords.latitude;
+    currentLocation.lng = position.coords.longitude;
+  });
+  moveCurrentPositionMarker()
+  addLayer()
+}
+
+function moveCurrentPositionMarker() {
+  marker = new google.maps.Marker({
+    position: currentLocation,
+    map,
+  });
+}
+
 // query 取ってくる
 const campId = window.location.search.slice(1).split("=")[1]
-
-let map;
 
 const campColors = {
   1: {
@@ -40,56 +59,102 @@ let campEncampedLocations = [
     longitude: 136.963427,
     camp_id: 2,
   },
+  {
+    id: 0,
+    latitude: 35.155725, 
+    longitude: 136.963427,
+    camp_id: 1,
+  },
+  {
+    id: 0,
+    latitude: 35.155725 + 0.0005, 
+    longitude: 136.963427 + 0.0005,
+    camp_id: 1,
+  },
+  {
+    id: 0,
+    latitude: 35.155725 + 0.0010, 
+    longitude: 136.963427 + 0.0010,
+    camp_id: 1,
+  },
+  {
+    id: 0,
+    latitude: 35.155725 + 0.0015, 
+    longitude: 136.963427 + 0.0015,
+    camp_id: 2,
+  },
 ];
 
 // MAPの初期化処理
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 35.155725, lng: 136.963427 },
-    zoom: 17,
-  });
-
-  // API叩いて一覧取得！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+  // API叩いて一覧取得
   all().then(data => {
     console.log(data)
-    // campEncampedLocations = data
+    data.map(datum => campEncampedLocations.push(datum))
   })
 
-  campEncampedLocations.forEach(layer => {
-    writeCircle(layer)
-  })
+  // 初期化
+  navigator.geolocation.getCurrentPosition(function location(position) {
+    currentLocation.lat = position.coords.latitude;
+    currentLocation.lng = position.coords.longitude;
+
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: currentLocation,
+      zoom: 17,
+    });
+
+    moveCurrentPositionMarker()
+
+    campEncampedLocations.forEach(layer => {
+      writeSquare(layer)
+    })
+  });
+
+  // setInterval("changeCurrentPosition()", 10000);
 }
 
 // 位置情報を追加して円を表示
 function addLayer() {
-  let layer = {
+  let camp_encamped_location = {
     latitude: 0,
     longitude: 0,
     camp_id: campId,
   }
   navigator.geolocation.getCurrentPosition(function show_location(position) {
-    layer.latitude = position.coords.latitude;
-    layer.longitude = position.coords.longitude;
+    camp_encamped_location.latitude = position.coords.latitude;
+    camp_encamped_location.longitude = position.coords.longitude;
 
-    writeCircle(layer)
-    campEncampedLocations.push(layer)
+    // 重なるやつないかチェック
+
+    writeSquare(camp_encamped_location)
+    campEncampedLocations.push(camp_encamped_location)
     // API叩いて作成！！！！！！！！！！！！！！！！！！！！！！！！！！
-    save(layer)
+    save(camp_encamped_location)
   });
 }
 
-// 円を表示
-function writeCircle(layer) {
-  const cityCircle = new google.maps.Circle({
+// 四角形を表示
+function genSquareCoords(location) {
+  return [
+    { lat: location.latitude + 0.0005, lng: location.longitude + 0.0005 },
+    { lat: location.latitude + 0.0005, lng: location.longitude - 0.0005 },
+    { lat: location.latitude - 0.0005, lng: location.longitude - 0.0005 },
+    { lat: location.latitude - 0.0005, lng: location.longitude + 0.0005 },
+    { lat: location.latitude + 0.0005, lng: location.longitude + 0.0005 },
+  ];
+}
+function writeSquare(layer) {
+  let squareCoords = genSquareCoords(layer)
+  console.log(squareCoords)
+  const square = new google.maps.Polygon({
+    paths: squareCoords,
     strokeColor: "#ffffff",
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: campColors[layer.camp_id].color,
     fillOpacity: 0.8,
-    map,
-    center: { lat: layer.latitude, lng: layer.longitude },
-    radius: 50,
   });
+  square.setMap(map);
 }
 
 
@@ -108,56 +173,4 @@ function deleteCEL(camp_encamped_location) {
   // credential周り設定したほうが良さそう。
   fetch( `http://localhost:8000/camp_encamped_locations/${camp_encamped_location.id}`, { method: "DELETE", mode: "cors" } )
   .then(resp => console.log(resp))
-}
-
-// API叩くクラスの宣言
-class CampEncampedLocation {
-  constructor(arg) {
-    this.init()
-    if ( typeof arg == "number" ) { this.fetch(arg) }
-    // if ( typeof arg == "object" ) { Object.assign(this, arg) }
-  }
-
-  init(){
-    this.camp_id = null,
-    this.latitude = null,
-    this.longitude = null
-  }
-
-  // 取得
-  fetch(id){
-    fetch(`http://localhost:8000/camp_encamped_locations/${id}`, {mode: 'cors'})
-    .then( resp => resp.json() )
-    .then( data => Object.assign(this, data.camp_encamped_location) )
-  }
-
-  // 全取得
-  async all() {
-    let resp = await fetch(`http://localhost:8000/camp_encamped_locations`, {mode: 'cors'})
-    return resp.json()
-  }
-
-  // 作成
-  create(object){ // {camp_id: Number, latitude: Number, longitude: Number}
-    this.init()
-    Object.assign(this, object)
-    this.save()
-  }
-
-  save() {
-    // credential周り設定したほうが良さそう。
-    fetch( 'http://localhost:8000/camp_encamped_locations', { method: "POST", body: this.toPostedObject(), mode: "cors" } )
-  }
-
-  toPostedObject(){
-    var postedObject = {}
-    postedObject.camp_id = this.camp_id
-    postedObject.latitude = this.latitude
-    postedObject.longitude = this.longitude
-    return JSON.stringify(postedObject)
-  }
-
-  // update(){
-
-  // }
 }
